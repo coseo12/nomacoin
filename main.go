6c1976f46ddf0fd0f19b8c2aaa269fe6b2a1dbfe
coseo12 <momo_12@naver.com>
2021-07-24 13:43:16 +0900

    add package explorer

--- main.go
+++ main.go
@@ -1,21 +1,7 @@
 package main
 
-import (
-	"fmt"
-
-	"github.com/coseo12/nomacoin/blockchain"
-)
+import explorer "github.com/coseo12/nomacoin/explorer/templates"
 
 func main() {
-	chain := blockchain.GetBlockchain()
-	chain.AddBlock("Second Block")
-	chain.AddBlock("Third Block")
-	chain.AddBlock("Fourth Block")
-
-	for _, block := range chain.AllBlocks() {
-		fmt.Printf("Data %s\n", block.Data)
-		fmt.Printf("Hash %s\n", block.Hash)
-		fmt.Printf("PrevHash %s\n", block.PrevHash)
-	}
-
+	explorer.Start()
 }
