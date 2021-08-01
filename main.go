51c20d0a3925c254216bc03a0881182bb74bb342
coseo12 <momo_12@naver.com>
2021-08-01 16:05:52 +0900

    Restoring the blockchain

--- main.go
+++ main.go
@@ -1,11 +1,7 @@
 package main
 
-import (
-	"github.com/coseo12/nomacoin/blockchain"
-)
+import "github.com/coseo12/nomacoin/blockchain"
 
 func main() {
-	blockchain.Blockchain().AddBlock("First")
-	blockchain.Blockchain().AddBlock("Second")
-	blockchain.Blockchain().AddBlock("Third")
+	blockchain.Blockchain()
 }
