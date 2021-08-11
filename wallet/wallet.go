d0ad4e937ba515d45fccc505282bab28e5cfff82
coseo12 <momo_12@naver.com>
2021-08-11 19:44:30 +0900

    Sigining Messages

--- wallet/wallet.go
+++ wallet/wallet.go
@@ -1,1 +1,28 @@
 package wallet
+
+import (
+	"crypto/ecdsa"
+	"crypto/elliptic"
+	"crypto/rand"
+	"encoding/hex"
+	"fmt"
+
+	"github.com/coseo12/nomacoin/utils"
+)
+
+func Start() {
+	privateKey, err := ecdsa.GenerateKey(elliptic.P256(), rand.Reader)
+	utils.HandleErr(err)
+
+	message := "I love you"
+
+	hashedMessage := utils.Hash(message)
+
+	hashAsByte, err := hex.DecodeString(hashedMessage)
+	utils.HandleErr(err)
+
+	r, s, err := ecdsa.Sign(rand.Reader, privateKey, hashAsByte)
+	utils.HandleErr(err)
+
+	fmt.Printf("R:%d\nS:%d\n", r, s)
+}
