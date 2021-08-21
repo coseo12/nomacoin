2e528f53f69d34e1208469589f490b30100c6942
coseo12 <momo_12@naver.com>
2021-08-21 14:33:41 +0900

    ReadMessage

--- p2p/p2p.go
+++ p2p/p2p.go
@@ -1,6 +1,7 @@
 package p2p
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/coseo12/nomacoin/utils"
@@ -10,6 +11,14 @@ import (
 var upgrader = websocket.Upgrader{}
 
 func Upgrade(w http.ResponseWriter, r *http.Request) {
-	_, err := upgrader.Upgrade(w, r, nil)
+	upgrader.CheckOrigin = func(r *http.Request) bool {
+		return true
+	}
+	conn, err := upgrader.Upgrade(w, r, nil)
 	utils.HandleErr(err)
+	for {
+		_, p, err := conn.ReadMessage()
+		utils.HandleErr(err)
+		fmt.Printf("%s\n", p)
+	}
 }
