08e1efaedddedb31f33c2b303499596e30c9570f
coseo12 <momo_12@naver.com>
2021-10-11 19:27:31 +0900

    Blockchain Test

--- blockchain/block_test.go
+++ blockchain/block_test.go
@@ -3,6 +3,8 @@ package blockchain
 import (
 	"reflect"
 	"testing"
+
+	"github.com/coseo12/nomacoin/utils"
 )
 
 func TestCreateBlock(t *testing.T) {
@@ -12,4 +14,32 @@ func TestCreateBlock(t *testing.T) {
 	if reflect.TypeOf(b) != reflect.TypeOf(&Block{}) {
 		t.Error("createBlock() should return an instance of a block")
 	}
+}
+
+func TestFindBlock(t *testing.T) {
+	t.Run("Block not found", func(t *testing.T) {
+		dbStorage = fakeDB{
+			fakeFindBlock: func() []byte {
+				return nil
+			},
+		}
+		_, err := FindBlock("xx")
+		if err == nil {
+			t.Error("The block should not be found")
+		}
+	})
+	t.Run("Block is found", func(t *testing.T) {
+		dbStorage = fakeDB{
+			fakeFindBlock: func() []byte {
+				b := &Block{
+					Height: 1,
+				}
+				return utils.ToBytes(b)
+			},
+		}
+		b, _ := FindBlock("xx")
+		if b.Height != 1 {
+			t.Error("The block should be found.")
+		}
+	})
 }
